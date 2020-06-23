b16b67d400a78fa37f873c3a4a1a8ae66a673a4f
Rodrigo Téllez Lazo <r.tellez91@outlook.com.pe>
2020-06-23 16:14:03 -0500

    Module 2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <book-list></book-list>
+    <BookList></BookList>
   </div>
 </template>
 
 <script>
-import { BookList } from "./components/BookList";
+import BookList from "./components/BookList";
 
 export default {
   name: "app",
